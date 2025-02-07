0a7470b1d5e3b17a9a49b4474eaa3a31a7fbc476
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-02-07 14:16:47 -0300

    Add font, tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+export default {
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        colors:{
+          primary: '#',
+          secondary: '#',
+          background: '#',
+          darkBackground: '#',
+          text: '#000000',
+          darkText: '#FFFFFF'
+        },
+        fontFamily: {
+          custom: ['Poppins', 'sans-serif'],
+        },
+      },
+    },
+    darkMode: 'class', // Dark mode support
+    plugins: [],
+  };
+  
