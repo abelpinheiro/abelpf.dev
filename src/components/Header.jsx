5fe3812700c1c32119c7d494fedc9f705568bb4b
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-02-08 17:34:54 -0300

    add header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className="flex justify-between p-4">
+      <div className="logo">
+        <h1>abelpf.dev</h1>
+      </div>
+      <nav className="nav ml-auto">
+        <ul className="flex space-x-4">
+          <li><a href="/" aria-label="Go to the homepage">Home</a></li>
+          <li><a href="/about" aria-label="Learn more about me">About</a></li>
+          <li><a href="/experience" aria-label="My professional experience">Experience</a></li>
+          <li><a href="/projects" aria-label="My personal projects">Projects</a></li>
+          <li><a href="/resume" aria-label="Read my resume">Resume</a></li>
+          <li><a href="/blog" aria-label="Go to my blogs">Blogs</a></li>
+        </ul>
+      </nav>
+    </header>
   )
 }
