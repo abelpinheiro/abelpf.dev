bc644d147cbf9e58edeced81fd8e60e922a15934
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-02-22 17:37:06 -0300

    routes, toggle theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './components/Home'
+import About from './components/About'
+import Experience from './components/Experience'
+import Projects from './components/Projects'
+import Resume from './components/Resume'
+import Blogs from './components/Blogs'
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-      <Footer />
-    </>
+    <ThemeProvider>
+      <Router basename="/abelpf.dev">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path='/about' element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/blogs" element={<Blogs />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </ThemeProvider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
 import { useState } from "react"
+import { useTheme } from "../context/ThemeContext";
 
 export default function Header() {
+  const { isDarkMode, toggleTheme } = useTheme();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const handleToggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   }
 
+
   return (
-    <header className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 bg-white text-black dark:bg-black dark:text-white">
+    <header className={`max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ${isDarkMode ? 'dark:bg-black dark:text-white' : 'bg-white text-black'}`}>
       <div className="font-poppins text-3xl">
         <h1>abelpf.dev</h1>
       </div>
@@ -28,14 +32,57 @@ export default function Header() {
 
       <nav className={`${isMenuOpen ? "block" : "hidden"} w-full md:block md:w-auto`} id="navbar-default">
         <ul className="text-xl flex flex-col font-medium p-4 md:p-0 mt-4 border border-orange-200 rounded-lg bg-orange-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-black md:dark:bg-black dark:border-orange-700">
-          <li><a href="/" aria-label="Go to the homepage" className="font-poppins block py-2 px-3 text-white bg-orange-500 rounded-sm md:bg-transparent md:text-orange-500 md:p-0 dark:text-white dark:bg-orange-500 md:dark:bg-transparent">Home</a></li>
-          <li><a href="/about" aria-label="Learn more about me" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">About</a></li>
-          <li><a href="/experience" aria-label="My professional experience" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">Experience</a></li>
-          <li><a href="/projects" aria-label="My personal projects" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">Projects</a></li>
-          <li><a href="/resume" aria-label="Read my resume" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">Resume</a></li>
-          <li><a href="/blog" aria-label="Go to my blogs" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">Blogs</a></li>
+          <li>
+            <Link to="/"  aria-label="Go to the homepage" className="font-poppins block py-2 px-3 text-white bg-orange-500 rounded-sm md:bg-transparent md:text-orange-500 md:p-0 dark:text-white dark:bg-orange-500 md:dark:bg-transparent">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" aria-label="Learn more about me" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to="/experience"aria-label="My professional experience" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">
+              Experience
+            </Link>
+          </li>
+          <li>
+            <Link to="/projects" aria-label="My personal projects" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">
+              Projects
+            </Link>
+          </li>
+          <li>
+            <Link to="/resume" aria-label="Read my resume" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link to="blogs" aria-label="Go to my blogs" className="font-poppins block py-2 px-3 text-black rounded-sm hover:bg-orange-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-500 dark:hover:bg-orange-800 md:dark:hover:bg-transparent">
+              Blogs
+            </Link>
+          </li>
         </ul>
       </nav>
+
+      <div>
+
+      {/* Botão de Toggle do Tema */}
+      <button
+        onClick={toggleTheme}
+        className="text-gray-700 dark:text-gray-200 hover:text-indigo-800 dark:hover:text-white focus:outline-none transition-colors duration-300"
+      >
+        {isDarkMode ? (
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+          </svg>
+        )}
+      </button>
+      </div>
     </header>
   )
 }
