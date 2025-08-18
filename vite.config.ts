1e1bef138543967ac9d8a2c5901ba0bddd6dd72d
abel pinheiro de figueiredo <abelpinheiro95@gmail.com>
2025-08-17 23:27:05 -0300

    fix local path finding

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/abelpf.dev/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    },
+    preserveSymlinks: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
